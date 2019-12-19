c1583e471a9412fe41f941438603a37ec8ac221d
bhimz <bhimz2001@gmail.com>
2019-12-19 19:33:51 +0700

    complete simple steps

--- app/src/androidTest/java/com/bhimz/bddsample/steps/LoginSteps.kt
+++ app/src/androidTest/java/com/bhimz/bddsample/steps/LoginSteps.kt
@@ -1,7 +1,13 @@
 package com.bhimz.bddsample.steps
 
+import androidx.test.espresso.Espresso.*
+import androidx.test.espresso.action.ViewActions.closeSoftKeyboard
+import androidx.test.espresso.matcher.ViewMatchers.*
+import androidx.test.espresso.action.ViewActions.*
+import androidx.test.espresso.assertion.ViewAssertions.*
 import androidx.test.rule.ActivityTestRule
 import com.bhimz.bddsample.LoginActivity
+import com.bhimz.bddsample.R
 import cucumber.api.java.en.Given
 import cucumber.api.java.en.Then
 import cucumber.api.java.en.When
@@ -25,32 +31,33 @@ class LoginSteps {
 
     @When("^I clicked on username field$")
     fun clickUserName() {
-
+        onView(withId(R.id.txtUsername)).perform(click())
     }
 
     @When("^I enter username \"([^\"]*)\"\$")
     fun inputUserName(username: String) {
-
+        onView(withId(R.id.txtUsername)).perform(typeText(username), closeSoftKeyboard())
     }
 
     @When("^I clicked on password field")
     fun clickPassword() {
-
+        onView(withId(R.id.txtPassword)).perform(click())
     }
 
     @When("I enter password \"([^\"]*)\"\$")
     fun inputPassword(password: String) {
+        onView(withId(R.id.txtPassword)).perform(typeText(password), closeSoftKeyboard())
 
     }
 
     @When("^I clicked login button")
     fun clickLogin() {
-
+        onView(withId(R.id.btnLogin)).perform(click())
     }
 
     //Asserts
     @Then("^I see successful login message$")
     fun assertLoginMessage() {
-
+        onView(withId(R.id.txtWelcome)).check(matches(isDisplayed()))
     }
 }
--- app/src/main/java/com/bhimz/bddsample/LoginActivity.kt
+++ app/src/main/java/com/bhimz/bddsample/LoginActivity.kt
@@ -2,11 +2,19 @@ package com.bhimz.bddsample
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import kotlinx.android.synthetic.main.activity_main.*
 
 class LoginActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        btnLogin.setOnClickListener {
+            if (txtPassword.text.isNullOrEmpty() || txtUsername.text.isNullOrEmpty()) {
+                return@setOnClickListener
+            }
+            txtWelcome.visibility = View.VISIBLE
+        }
     }
 }
